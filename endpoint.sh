e1d173af01733097038d3ce42a00f83495609c67
bingjun.nan <bingjun.nan@accenture.com>
2020-05-20 18:29:18 +0900

    [modify] refs #0010 upd

--- endpoint.sh
+++ endpoint.sh
@@ -13,7 +13,7 @@ done
 echo
 
 export JAVA_OPTS="-Dfile.encoding=UTF-8"
-export JMX_OPTS="-Djava.rmi.server.hostname=192.168.100.102 -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.rmi.port=9010 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+export JMX_OPTS="-Djava.rmi.server.hostname=${HOSTNAME} -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.rmi.port=9010 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 export DDG_OPTS="-Ddd.service.name=demo -Ddd.integrations.enabled=true -Ddd.trace.analytics.enabled=true -javaagent:/usr/jar/dd-java-agent.jar"
 
 echo "java ${JAVA_OPTS} ${JMX_OPTS} ${DDG_OPTS} -jar ${APP_JAR} ${args}"
